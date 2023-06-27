0d39a0e729647029a0887d0c302076f875a79816
Binit Kumar Shirsath <binitkumar0911@gmail.com>
2023-06-27 14:28:05 +0530

    BaseURL

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
+axios.defaults.baseURL="https://khanakhanabackend.onrender.com";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
